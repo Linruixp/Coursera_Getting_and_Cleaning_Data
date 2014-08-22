26ae3768972e123ebcb8f644a85df9518046706f
Linruixp <linruixp@gmail.com>
2014-08-22 23:07:49 +0800

    Eidt some comments.

--- run_analysis.R
+++ run_analysis.R
@@ -9,21 +9,22 @@ data <- rbind(train, test)
 # Step 2 - Extracts only the measurements on the mean and standard deviation for each measurement. 
 # reads features.txt into R
 features <- read.table("features.txt", head = FALSE, sep = "")
-# selecting features whose name with "mean" or "std", storing thier number into a vector called filter
+# selecting column names(measurenments) whose name with "mean" or "std", storing thier number into a vector called filter
 filter <- c(grep("mean", features$V2), grep("std", features$V2))
 filter <- sort(filter)
-# extracts variables from data set which matche the filter 
+# extracts variables from data set which match the filter 
 filtered_vars <- paste("V", filter, sep="")
 extracted_data <- data[filtered_vars]
 
 # Step 3 - Uses descriptive activity names to name the activities in the data set
-# read the training  and test labels into R, and combined them into a data frame which is named 'label'
+# read the training  and test labels into R, and combined them into a data frame which is named 'labels'
 y_train <- read.table("./train/y_train.txt", head = FALSE, sep = "")
 y_test <- read.table("./test/y_test.txt", head = FALSE, sep = "")
 labels <- rbind(y_train, y_test)
 # read activity names into R
 activity_names <- read.table("activity_labels.txt", head = FALSE, sep = "")
 # attach activity names by merge labels and actitity_names
+
 # codes below will use join method of 'plyr' package
 install.packages("plyr")
 library(plyr)
@@ -32,6 +33,7 @@ labels <- join(labels, activity_names, by = "V1", type = "left")
 data$activity <- labels$V2
 
 # Step 4 - Appropriately labels the data set with descriptive variable names.
+# the second variable V2 of data frame 'features' contains the descriptive variable names
 # codes below will use setnames method of 'data.table' package
 install.packages("data.table")
 library(data.table)
@@ -44,7 +46,7 @@ subject_test <- read.table("./test/subject_test.txt" ,head = FALSE, sep = "")
 subject <- rbind(subject_train, subject_test)
 # link subjects with observations by adding a new variable 'subject' to data set
 data$subject <- subject[[1]]
-# get mean of each variables in each categorty (actiity*subject)
+# get mean of each variables in each categorty (group by actiity*subject)
 aggdata <- aggregate(t[1:561], by=list(t$subject, t$activity), FUN=mean)
 # rename the first and second variables(Group.1 and Group.2)  to subject and activity, and get tidy data
 tidy_data <- rename(aggdata, c("Group.1"="subject", "Group.2"="activity"))
